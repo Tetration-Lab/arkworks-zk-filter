2e0e80e4366f5652df23101ccbfc54741121f935
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-04-25 15:44:22 +0700

    test: add filter trait test

--- src/bloom/traits.rs
+++ src/bloom/traits.rs
@@ -66,3 +66,47 @@ where
         parameter.contains(&to_bytes!(key.borrow()).expect("Failed to convert to bytes"))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use ark_bn254::Fr;
+    use ark_ff::{BigInteger, One, PrimeField, Zero};
+    use arkworks_mimc::{
+        params::{
+            mimc_7_91_bn254::{MIMC_7_91_BN254_PARAMS, MIMC_7_91_BN254_ROUND_KEYS},
+            round_keys_contants_to_vec,
+        },
+        MiMC, MiMCNonFeistelCRH,
+    };
+
+    use crate::{bloom::BloomFilter, Filter};
+
+    const BITS: usize = 253;
+    const N_HASH: usize = 2;
+    type BloomFilterTest =
+        BloomFilter<BITS, N_HASH, Fr, MiMCNonFeistelCRH<Fr, MIMC_7_91_BN254_PARAMS>>;
+
+    #[test]
+    fn filter_trait_works() -> Result<(), Box<dyn Error>> {
+        let mut bloom_filter = BloomFilterTest::new(MiMC::new(
+            1,
+            Fr::zero(),
+            round_keys_contants_to_vec(&MIMC_7_91_BN254_ROUND_KEYS),
+        ));
+        let mut another_bloom_filter = bloom_filter.clone();
+
+        let one = Fr::one();
+
+        <BloomFilterTest as Filter<Fr>>::insert(&mut bloom_filter, &one);
+        another_bloom_filter.insert(&one.into_repr().to_bytes_le());
+
+        assert_eq!(
+            bloom_filter.bits, another_bloom_filter.bits,
+            "Bits should be equal"
+        );
+
+        Ok(())
+    }
+}
