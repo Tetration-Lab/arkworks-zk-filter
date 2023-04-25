ddaf42ab85143bc99ead34587802e6dbd8c8c79a
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-04-25 17:08:14 +0700

    fix: correct take index constraint and add test

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use ark_ff::{PrimeField, ToBytes};
+use ark_ff::{to_bytes, PrimeField, ToBytes};
 use ark_relations::r1cs::SynthesisError;
 use ark_std::log2;
 
@@ -8,23 +8,16 @@ use ark_r1cs_std::{fields::fp::FpVar, R1CSVar, ToBytesGadget};
 /// Read value as bits in LE and take the first `max` bits then wraps around `max`.
 pub fn take_bits<T: ToBytes>(value: &T, max: usize) -> usize {
     let n_bits = log2(max) as usize;
-    let mut bytes = vec![];
-    value.write(&mut bytes).expect("Failed to write to bytes");
+    let bytes = to_bytes!(value).expect("Failed to convert to bytes");
     let sum: usize = bytes
         .iter()
-        .rev()
-        .flat_map(|b| (0..8).map(move |i| (b >> i) & 1 == 1))
+        .flat_map(|b| (0..8).map(move |i| (b >> i) & 1u8 == 1u8))
         .take(n_bits)
-        .fold(0, |acc, b| {
-            (acc << 1)
-                + match b {
-                    true => 1,
-                    false => 0,
-                }
-        });
+        .fold(0, |acc, b| (acc << 1) | (b as usize));
 
-    match sum > max - 1 {
-        true => sum - max,
+    let max_index = max - 1;
+    match sum > max_index {
+        true => sum - max_index,
         false => sum,
     }
 }
@@ -40,14 +33,45 @@ pub fn take_bits_gadget<F: PrimeField, T: ToBytesGadget<F> + R1CSVar<F>>(
     use ark_r1cs_std::{prelude::FieldVar, ToBitsGadget};
 
     let n_bits = log2(max) as usize;
-    let max_index = FpVar::constant(F::from(max as u64 - 1));
+    let two = FpVar::constant(F::from(2u64));
     let bits = value.to_bytes()?.to_bits_le()?;
-    //let mut index = FpVar::new_witness(ns!(value.cs(), "index"), || Ok(F::zero()))?;
     let mut index = FpVar::zero();
     for bit in bits.into_iter().take(n_bits) {
-        index = &index + &FpVar::from(bit);
+        index = &index * &two + &FpVar::from(bit);
     }
+
+    let max_index = FpVar::constant(F::from((max as u64) - 1));
     index
         .is_cmp(&max_index, Ordering::Greater, false)?
         .select(&(&index - &max_index), &index)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use ark_bn254::Fr;
+    use ark_ff::UniformRand;
+    use ark_r1cs_std::{fields::fp::FpVar, prelude::AllocVar, R1CSVar};
+    use ark_relations::r1cs::ConstraintSystem;
+    use ark_std::test_rng;
+
+    use super::{take_bits, take_bits_gadget};
+
+    #[test]
+    fn correct_bits() -> Result<(), Box<dyn Error>> {
+        let rng = &mut test_rng();
+        let cs = ConstraintSystem::<Fr>::new_ref();
+
+        let value = Fr::rand(rng);
+        let value_var = FpVar::new_witness(cs, || Ok(value))?;
+
+        let take = 9usize;
+        let taken = take_bits(&value, take);
+        let taken_var = take_bits_gadget(&value_var, take)?;
+
+        assert_eq!(Fr::from(taken as u64), taken_var.value()?);
+
+        Ok(())
+    }
 }
